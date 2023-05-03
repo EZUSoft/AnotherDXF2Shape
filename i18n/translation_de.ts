ab3df9c5bdc4af4e1babc892f7132e4958405133
Mike <EZUSoft@users.noreply.github.com>
2023-05-03 11:04:56 +0200

    Add files via upload

--- i18n/translation_de.ts
+++ i18n/translation_de.ts
@@ -14,19 +14,10 @@
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;EZUSoft: &lt;/span&gt;DXF Import / DXF2Shape Converter &lt;/p&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a name=&quot;result_box&quot;&gt;&lt;/a&gt;Import DXF files and optional storage as Shape&lt;/p&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Copyright (c) 2016-2020 Mike Blechschmidt  &lt;a href=&quot;mailto:qgis@makobo.de&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;qgis@makobo.de&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;EZUSoft: &lt;/span&gt;
-DXF Import / DXF2Shape Konverter&lt;/p&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;
-&lt;span id=&quot;result_box&quot; class=&quot;short_text&quot; lang=&quot;en&quot;&gt;&lt;span&gt;Import&lt;/span&gt; &lt;span&gt;von DXF Dateien &lt;/span&gt;
-und optionale Speicherung als &lt;span&gt;Shape&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
-&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Copyright (c) 2016-2018 Mike Blechschmidt  &lt;a href=&quot;mailto:qgis@makobo.de&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;qgis@makobo.de&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt; {3C?} {4.0/?} {3.?} {40/?} {1&quot;?} {2&apos;?} {8p?} {400;?} {12p?} {12p?} {0p?} {0p?} {0;?} {0p?} {600;?} {2S?} {12p?} {12p?} {0p?} {0p?} {0;?} {0p?} {12p?} {12p?} {0p?} {0p?} {0;?} {0p?} {2016-2020 ?} {0000f?}</translation>
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;EZUSoft: &lt;/span&gt;DXF Import / DXF2Shape&amp;amp;GeoPackage Converter &lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a name=&quot;result_box&quot;&gt;&lt;/a&gt;Import DXF files and optional storage as Shape or Geopackage (QGIS 3.0)&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Copyright (c) 2016-2023 Mike Blechschmidt  &lt;a href=&quot;mailto:qgis@makobo.de&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;qgis@makobo.de&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../uiAbout.ui" line="98"/>
@@ -47,57 +38,63 @@ p, li { white-space: pre-wrap; }
         <translation>EZUSoft: DXF Import / DXF2Shape Konverter</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="80"/>
+        <location filename="../uiADXF2Shape.ui" line="86"/>
         <source>Input DXF-File</source>
         <translation>DXF Quelldatei</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="91"/>
+        <location filename="../uiADXF2Shape.ui" line="97"/>
         <source>.. per code ...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="98"/>
+        <location filename="../uiADXF2Shape.ui" line="104"/>
         <source>Save as shape-files</source>
         <translation>Shape-Dateien speichern</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="114"/>
+        <location filename="../uiADXF2Shape.ui" line="120"/>
         <source>...code ...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="621"/>
+        <location filename="../uiADXF2Shape.ui" line="386"/>
+        <source>Replace tab</source>
+        <translation>Ersetze Tabulator</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="663"/>
         <source>Import</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../uiADXF2Shape.ui" line="33"/>
-        <location filename="../uiADXF2Shape.ui" line="684"/>
-        <location filename="../uiADXF2Shape.ui" line="775"/>
+        <location filename="../uiADXF2Shape.ui" line="751"/>
+        <location filename="../uiADXF2Shape.ui" line="774"/>
+        <location filename="../uiADXF2Shape.ui" line="865"/>
         <source>TextLabel</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="220"/>
+        <location filename="../uiADXF2Shape.ui" line="226"/>
         <source>group layer
 (takes a long time)</source>
         <translation>Layer einzeln gruppieren
 (zeitaufwändig)</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="67"/>
-        <location filename="../uiADXF2Shape.ui" line="130"/>
+        <location filename="../uiADXF2Shape.ui" line="73"/>
+        <location filename="../uiADXF2Shape.ui" line="136"/>
         <source>Browse</source>
         <translation>Durchsuche</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="338"/>
+        <location filename="../uiADXF2Shape.ui" line="314"/>
         <source>Charset</source>
         <translation>Zeichensatz</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="234"/>
+        <location filename="../uiADXF2Shape.ui" line="240"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If this option is enabled, merging blocks will convert into a geometry collection.&lt;/p&gt;&lt;p&gt;If there is a symbol of the polygon &lt;span style=&quot; font-weight:600;&quot;&gt;and&lt;/span&gt; line can not be generated by this symbol, because ESRI shapefiles can only store one kind of geometry per layer.&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;Ist diese Option aktiviert, dann wird versucht, Geometieblöcke (z.B. Signaturen und Schraffuren) 
 als zusammenhängende Objekte zu erzeugen.&lt;br&gt;Besteht aber z.B eine Signatur/Symbol aus einem Polygon 
@@ -106,47 +103,47 @@ Geometrietypen nicht generiert werden.
 &lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="272"/>
+        <location filename="../uiADXF2Shape.ui" line="265"/>
         <source>Text formating codes</source>
         <translation>Text Format Code</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="282"/>
+        <location filename="../uiADXF2Shape.ui" line="281"/>
         <source>hide and use </source>
         <translation>Ausblenden/Anwenden</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="311"/>
+        <location filename="../uiADXF2Shape.ui" line="294"/>
         <source>Use Color attribut from DXF</source>
         <translation>Farbattribut aus DXF verwenden</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="331"/>
+        <location filename="../uiADXF2Shape.ui" line="354"/>
         <source>Label(Point)</source>
         <translation>Beschriftung(Punkt)</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="348"/>
+        <location filename="../uiADXF2Shape.ui" line="347"/>
         <source>Line</source>
         <translation>Linie</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="355"/>
+        <location filename="../uiADXF2Shape.ui" line="324"/>
         <source>Polygon</source>
         <translation>Fläche</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="378"/>
+        <location filename="../uiADXF2Shape.ui" line="373"/>
         <source>Factor</source>
         <translation>Faktor</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="651"/>
+        <location filename="../uiADXF2Shape.ui" line="683"/>
         <source>Reset</source>
         <translation>Zurücksetzen</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="240"/>
+        <location filename="../uiADXF2Shape.ui" line="246"/>
         <source>use geometry collection 
 (some elements may be lost)
 </source>
@@ -154,89 +151,95 @@ Geometrietypen nicht generiert werden.
 (einige Elemente könnten fehlen)</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="405"/>
+        <location filename="../uiADXF2Shape.ui" line="437"/>
         <source>Transformation settings</source>
         <translation>Transformationseinstellungen</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="162"/>
+        <location filename="../uiADXF2Shape.ui" line="168"/>
         <source>Drawing settings</source>
         <translation>Zeichnungseinstellungen</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="137"/>
+        <location filename="../uiADXF2Shape.ui" line="143"/>
         <source>Save as GeoPackage</source>
         <translation>Als GeoPackage speichern</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="168"/>
+        <location filename="../uiADXF2Shape.ui" line="181"/>
         <source>Use coordinate transformation</source>
         <translation>Koordinatentransformation aktivieren</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="175"/>
+        <location filename="../uiADXF2Shape.ui" line="174"/>
         <source>Use 3D if possible</source>
         <translation>Verwende 3D (wenn möglich)</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="421"/>
+        <location filename="../uiADXF2Shape.ui" line="453"/>
         <source>Type of input</source>
         <translation>Eingabeart</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="427"/>
+        <location filename="../uiADXF2Shape.ui" line="459"/>
         <source>Parameterset</source>
         <translation>Parameter</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="437"/>
+        <location filename="../uiADXF2Shape.ui" line="469"/>
         <source>Worldfile(*.wld)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="444"/>
+        <location filename="../uiADXF2Shape.ui" line="476"/>
         <source>Indentical Points</source>
         <translation>Indentische Punkte</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="470"/>
+        <location filename="../uiADXF2Shape.ui" line="502"/>
         <source>Parameter</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="478"/>
+        <location filename="../uiADXF2Shape.ui" line="510"/>
         <source>X Offset</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="495"/>
+        <location filename="../uiADXF2Shape.ui" line="527"/>
         <source>Y Offset</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="512"/>
+        <location filename="../uiADXF2Shape.ui" line="544"/>
         <source>Scale 1: </source>
         <translation>Maßstab 1:</translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="553"/>
+        <location filename="../uiADXF2Shape.ui" line="585"/>
         <source>ungeoref X</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="558"/>
+        <location filename="../uiADXF2Shape.ui" line="590"/>
         <source>ungeoref Y</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="563"/>
+        <location filename="../uiADXF2Shape.ui" line="595"/>
         <source>georef X</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../uiADXF2Shape.ui" line="568"/>
+        <location filename="../uiADXF2Shape.ui" line="600"/>
         <source>georef Y</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="696"/>
+        <location filename="../uiADXF2Shape.ui" line="699"/>
+        <source>Donation for our passion project</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
